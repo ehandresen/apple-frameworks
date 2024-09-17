b2f6f2b2a3f4b007a12d1734c3f6991e22251cc2
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-09-17 22:30:28 +0200

    Initial Commit

---
+++ apple-frameworks/apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworksApp.swift
+//  apple-frameworks
+//
+//  Created by Eirik Andresen on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
