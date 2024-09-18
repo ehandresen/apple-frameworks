373594bf3c54e0bec1576668ef34e2e21bbc0c5c
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-09-18 22:02:28 +0200

    FrameworkTitleView, Grid LazyVGrid

--- apple-frameworks/FrameworkGridView.swift
+++ apple-frameworks/FrameworkGridView.swift
@@ -9,19 +9,11 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     
+    let columns: [GridItem] = [GridItem(.flexible())]
+    
     var body: some View {
-        VStack {
-            Image("app-clip")
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text("App Clips")
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-            
-            }
-        }
+        FrameworkTitleView(appName: "App Clips", imageName: "app-clip")
+    }
 }
 
 #Preview {
@@ -29,15 +21,15 @@ struct FrameworkGridView: View {
 }
 
 struct FrameworkTitleView : View {
-    let name: String
+    let appName: String
     let imageName: String
     
     var body: some View {
         VStack {
-            Image("app-clip")
+            Image(imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text("App Clips")
+            Text(appName)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
